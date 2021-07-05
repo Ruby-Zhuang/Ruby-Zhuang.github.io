047778009ca8b244f36380a805d7de5ebcf09a90
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-07-05 17:49:30 -0400

    Add function for switching active links while scrolling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,3 +26,32 @@ const linkAction = () => {
 };
 
 navLinks.forEach((link) => link.addEventListener('click', linkAction));
+
+/******************************
+ * SCROLL SECTIONS ACTIVE LINK
+ ******************************/
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', scrollActive);
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionBottom = sectionTop + sectionHeight;
+
+    sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionBottom) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active');
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active');
+    }
+  });
+};
