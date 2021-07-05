6853ff564a2df006fd3cce9de927a9977129264d
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-07-05 17:34:15 -0400

    Add functionality to toggle showing menu when a nav item is clicked

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,5 @@
 /******************************
- * SHOW MENU
+ * SHOW NAV MENU
  ******************************/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId);
@@ -13,3 +13,16 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu('nav-toggle', 'nav-menu');
+
+/******************************
+ * REMOVE NAV MENU MOBILE
+ ******************************/
+const navLinks = document.querySelectorAll('.nav__link');
+
+// Toggle Navbar menu when a menu item link is clicked
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.toggle('show');
+};
+
+navLinks.forEach((link) => link.addEventListener('click', linkAction));
