9351ac9f099dde188846e5cd61040fa7a6db6419
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-07-08 16:31:12 -0400

    Add scroll reveal animations

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -59,3 +59,36 @@ const scrollActive = () => {
 };
 
 window.addEventListener('scroll', scrollActive);
+
+/******************************
+ * SCROLL REVEAL ANIMATION
+ ******************************/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true,
+});
+
+// Scroll Home
+sr.reveal('.home__title', {});
+sr.reveal('.home__scroll', { delay: 200 });
+sr.reveal('.home__img', { origin: 'right', delay: 400 });
+
+// Scroll About
+sr.reveal('.about__img', { delay: 500 });
+sr.reveal('.about__subtitle', { delay: 300 });
+sr.reveal('.about__profession', { delay: 400 });
+sr.reveal('.about__text', { delay: 500 });
+sr.reveal('.about__social-icon', { delay: 600, interval: 200 });
+
+// Scroll Skills
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 75 });
+
+// Scroll Projects
+sr.reveal('.project__img', { interval: 200 });
+
+// Scroll Contact
+sr.reveal('.contact__subtitle', {});
+sr.reveal('.contact__text', { interval: 200 });
